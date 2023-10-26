505ba98857ef41a35d014e958238207ebdaa24f9
RGB-Grey-Chrome <129196368+seriybeliy11@users.noreply.github.com>
2023-10-26 13:28:08 +0300

    Add files via upload

--- Tracker.py
+++ Tracker.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import time
+import platform
 
 npm_command = "npm run dev"
 python_script_path = os.path.join("public", "get_data.py")
@@ -8,8 +9,17 @@ python_script_path = os.path.join("public", "get_data.py")
 try:
     npm_process = subprocess.Popen(npm_command, shell=True)
     time.sleep(5)
-    os.chdir("public")
-    python_process = subprocess.Popen("python get_data.py", shell=True)
+
+    os_type = platform.system()
+
+    if os_type == "Darwin":
+        python_process = subprocess.Popen("python get_data.py", shell=True)
+    elif os_type == "Linux":
+        python_process = subprocess.Popen("python3 get_data.py", shell=True)
+    elif os_type == "Windows":
+        python_process = subprocess.Popen("python get_data.py", shell=True)
+    else:
+        raise OSError(f"Unsupported operating system: {os_type}")
 
     npm_process.wait()
     python_process.wait()

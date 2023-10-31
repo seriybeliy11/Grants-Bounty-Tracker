2fd8c02270b835c47c95ffbed08d144787b26571
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-31 19:11:09 +0300

    Refactor issue_comment_parser.py

--- parsers/issue_comment_parser.py
+++ parsers/issue_comment_parser.py
@@ -5,19 +5,51 @@ import nats
 import redis
 import json
 
-async def main():
+def get_github_token():
     GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
     if GITHUB_TOKEN is None:
         raise ValueError("The environment variable GITHUB_TOKEN is not set.")
 
+    return GITHUB_TOKEN
+
+def get_issues(GITHUB_TOKEN, url, params, page):
     HEADERS = {
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
     }
+    params["page"] = page
+    response = requests.get(url, headers=HEADERS, params=params)
+    response.raise_for_status()
+    return response.json()
 
+async def get_comment_count(GITHUB_TOKEN, issue_url):
+    HEADERS = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    comments_url = issue_url + "/comments"
+    response = await asyncio.to_thread(requests.get, comments_url, headers=HEADERS)
+    response.raise_for_status()
+    return len(response.json())
+
+def save_data_to_redis(redis_client, key, data, expiration):
+    redis_client.setex(key, expiration, json.dumps(data))
+
+def get_data_from_redis(redis_client, key):
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None
+
+async def send_data_to_nats(topic, data):
+    nc = await nats.connect("nats")
+    await nc.publish(topic, data.encode())
+    await nc.close()
+
+async def main():
+    GITHUB_TOKEN = get_github_token()
     url = "https://api.github.com/repos/ton-society/grants-and-bounties/issues"
-
     params = {
         "per_page": 100,
         "state": "all"
@@ -25,35 +57,23 @@ async def main():
 
     redis_conn = redis.StrictRedis(host='redis', port=6379, db=0)
 
-    async def get_issues(page):
-        params["page"] = page
-        response = await asyncio.to_thread(requests.get, url, headers=HEADERS, params=params)
-        response.raise_for_status()
-        return response.json()
-
-    async def get_comment_count(issue_url):
-        comments_url = issue_url + "/comments"
-        response = await asyncio.to_thread(requests.get, comments_url, headers=HEADERS)
-        response.raise_for_status()
-        return len(response.json())
-
-    async def collect_issue_comments():
-        cached_data = redis_conn.get('issue_comments')
-        if cached_data:
-            return json.loads(cached_data)
-
+    cached_data = get_data_from_redis(redis_conn, 'issue_comments')
+    
+    if cached_data:
+        result = cached_data
+    else:
         issues_with_comments = []
 
         page = 1
         while True:
-            issues = await get_issues(page)
+            issues = get_issues(GITHUB_TOKEN, url, params, page)
             if not issues:
                 break
 
             tasks = []
             for issue in issues:
                 issue_url = issue["url"]
-                tasks.append(get_comment_count(issue_url))
+                tasks.append(get_comment_count(GITHUB_TOKEN, issue_url))
 
             comment_counts = await asyncio.gather(*tasks)
 
@@ -64,19 +84,11 @@ async def main():
 
             page += 1
 
-        redis_conn.setex('issue_comments', 4 * 60 * 60, json.dumps(issues_with_comments))
+        save_data_to_redis(redis_conn, 'issue_comments', issues_with_comments, 4 * 60 * 60)
 
-        return issues_with_comments
+        result = issues_with_comments
 
-    async def send_result_to_nats(result):
-        nc = await nats.connect("nats")
-
-        await nc.publish("issue_comments", result.encode())
-
-        await nc.close()
-
-    issues_result = await collect_issue_comments()
-    await send_result_to_nats(issues_result)
+    await send_data_to_nats("issue_comments", json.dumps(result))
 
 if __name__ == '__main__':
     asyncio.run(main())

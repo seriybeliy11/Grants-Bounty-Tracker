4de26159cfc12991914c246649bc9212de865c34
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-31 19:14:59 +0300

    Refactor labels_stats_parser.py - Added get_github_token() function to retrieve GitHub token from environment variables. - Added get_issues(), save_data_to_redis(), get_data_from_redis(), and send_data_to_nats() functions. - Refactored the main() function to use these new functions for improved readability and maintainability. - Replaced direct API requests with get_issues() function. - Cached labels statistics data in Redis for better performance. - Published labels statistics data to NATS.

--- parsers/labels_stats_parser.py
+++ parsers/labels_stats_parser.py
@@ -5,40 +5,60 @@ import json
 import nats
 import redis
 
-async def main():
+def get_github_token():
     GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
     if GITHUB_TOKEN is None:
-        raise ValueError("issue_type")
+        raise ValueError("The environment variable GITHUB_TOKEN is not set.")
+    
+    return GITHUB_TOKEN
 
+def get_issues(url, params, headers, page):
+    params["page"] = page
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+def save_data_to_redis(redis_client, key, data, expiration):
+    redis_client.setex(key, expiration, json.dumps(data))
+
+def get_data_from_redis(redis_client, key):
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None
+
+async def send_data_to_nats(topic, data):
+    nc = await nats.connect("nats")
+    await nc.publish(topic, data.encode())
+    await nc.close()
+
+async def main():
+    GITHUB_TOKEN = get_github_token()
     HEADERS = {
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
     }
-
     url = "https://api.github.com/repos/ton-society/grants-and-bounties/issues"
-
     params = {
         "per_page": 100,
         "state": "all"
     }
 
-    redis_host = "redis"
-    redis_port = 6379
-    redis_db = redis.StrictRedis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+    redis_client = redis.StrictRedis(host='redis', port=6379, db=0, decode_responses=True)
 
-    cached_data = redis_db.get("labels_stats")
+    cached_data = get_data_from_redis(redis_client, "labels_stats")
 
     if cached_data:
-        labels_result = json.loads(cached_data)
+        labels_result = cached_data
     else:
         labels_count = {}
 
-        def get_issues(page):
-            params["page"] = page
-            response = requests.get(url, headers=HEADERS, params=params)
-            response.raise_for_status()
-            issues = response.json()
+        page = 1
+        while True:
+            issues = get_issues(url, params, HEADERS, page)
+            if not issues:
+                break
 
             for issue in issues:
                 labels = issue["labels"]
@@ -46,26 +66,13 @@ async def main():
                     label_name = label["name"]
                     labels_count[label_name] = labels_count.get(label_name, 0) + 1
 
-            return issues
-
-        page = 1
-        while True:
-            issues = get_issues(page)
-            if not issues:
-                break
-
             page += 1
 
         labels_result = [{"label": label, "value": count} for label, count in labels_count.items()]
 
-        redis_db.setex("labels_stats", 4 * 3600, json.dumps(labels_result))
+        save_data_to_redis(redis_client, "labels_stats", labels_result, 4 * 3600)
 
-
-    nc = await nats.connect("nats")
-
-    await nc.publish("labels_stats", labels_result.encode())
-
-    await nc.close()
+    await send_data_to_nats("labels_stats", json.dumps(labels_result))
 
 if __name__ == '__main__':
     asyncio.run(main())

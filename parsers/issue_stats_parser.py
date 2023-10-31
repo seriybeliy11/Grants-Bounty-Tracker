b84614252ca77818861ff53cf48e22b817e909a5
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-31 19:13:00 +0300

    Refactor issue_stats_parser.py: - Added  function. - Added , , , and  functions. - Removed duplicate code and reorganized the main function. - Improved data retrieval and caching. - Refactored data publication to NATS.

--- parsers/issue_stats_parser.py
+++ parsers/issue_stats_parser.py
@@ -6,11 +6,45 @@ import nats
 import redis
 from datetime import datetime, timedelta
 
-async def main():
+def get_github_token():
     GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+
     if GITHUB_TOKEN is None:
         raise ValueError("Environment variable GITHUB_TOKEN is not set.")
+    
+    return GITHUB_TOKEN
 
+def get_issues(page, url, params, headers):
+    params["page"] = page
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+def calculate_duration(issue):
+    if issue["state"] == "open":
+        created_at = datetime.fromisoformat(issue["created_at"][:-1])
+        now = datetime.now()
+        duration = (now - created_at).days
+    else:
+        duration = 0.0
+    return duration
+
+def save_data_to_redis(redis_client, key, data, expiration):
+    redis_client.setex(key, expiration, json.dumps(data))
+
+def get_data_from_redis(redis_client, key):
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None
+
+async def send_data_to_nats(topic, data):
+    nc = await nats.connect("nats")
+    await nc.publish(topic, data.encode())
+    await nc.close()
+
+async def main():
+    GITHUB_TOKEN = get_github_token()
     HEADERS = {
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
@@ -22,37 +56,17 @@ async def main():
         "state": "all"
     }
 
-    def get_issues(page):
-        params["page"] = page
-        response = requests.get(url, headers=HEADERS, params=params)
-        response.raise_for_status()
-        return response.json()
-
-    def calculate_duration(issue):
-        if issue["state"] == "open":
-            created_at = datetime.fromisoformat(issue["created_at"][:-1])
-            now = datetime.now()
-            duration = (now - created_at).days
-        else:
-            duration = 0.0
-        return duration
-
     redis_client = redis.Redis(host='redis', port=6379, db=0)
 
-    duration_by_year = {}
-
-    async def send_result_to_nats(result):
-        nc = await nats.connect("nats")
-        await nc.publish("issue_stats", result.encode())
-        await nc.close()
-
-    cached_result = redis_client.get("issue_stats")
+    cached_result = get_data_from_redis(redis_client, "issue_stats")
     if cached_result is not None:
-        result = cached_result.decode('utf-8')
+        result = cached_result
     else:
         page = 1
+        duration_by_year = {}
+
         while True:
-            issues = get_issues(page)
+            issues = get_issues(page, url, params, HEADERS)
             if not issues:
                 break
 
@@ -65,9 +79,9 @@ async def main():
             page += 1
 
         result = [{year: issues} for year, issues in duration_by_year.items()]
-        redis_client.setex("issue_stats", timedelta(hours=4), json.dumps(result))
+        save_data_to_redis(redis_client, "issue_stats", result, timedelta(hours=4))
 
-    await send_result_to_nats(result)
+    await send_data_to_nats("issue_stats", json.dumps(result))
 
 if __name__ == '__main__':
     asyncio.run(main())

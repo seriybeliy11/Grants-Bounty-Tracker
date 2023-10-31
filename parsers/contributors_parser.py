57afcbbe5011ff23901634893018a5fc9695046f
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-31 19:07:52 +0300

    Refactor contributors_parser.py: - Added get_github_token function. - Added get_issues, save_data_to_redis, get_data_from_redis, send_data_to_nats functions. - Modified get_contributors function to use get_github_token and refactored data retrieval and caching. - Modified main function to use await for get_contributors and send_data_to_nats.

--- parsers/contributors_parser.py
+++ parsers/contributors_parser.py
@@ -2,28 +2,50 @@ import os
 import requests
 import asyncio
 import nats
-import redis
 import json
+import redis
 
-def get_contributors():
+def get_github_token():
     GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
     if GITHUB_TOKEN is None:
         raise ValueError("The environment variable GITHUB_TOKEN is not set.")
 
+    return GITHUB_TOKEN
+
+def get_issues(url, params, headers, page):
+    params["page"] = page
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+def save_data_to_redis(redis_client, key, data, expiration):
+    redis_client.setex(key, expiration, json.dumps(data))
+
+def get_data_from_redis(redis_client, key):
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None
+
+async def send_data_to_nats(topic, data):
+    nc = await nats.connect("nats")
+    await nc.publish(topic, data.encode())
+    await nc.close()
+
+async def get_contributors():
+    GITHUB_TOKEN = get_github_token()
     headers = {
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
     }
-
     url = "https://api.github.com/repos/ton-society/grants-and-bounties/contributors"
-
     redis_conn = redis.StrictRedis(host='redis', port=6379, db=0)
 
-    contributors_data = redis_conn.get('github_contributors')
+    contributors_data = get_data_from_redis(redis_conn, 'github_contributors')
 
     if contributors_data:
-        contributors_list = json.loads(contributors_data.decode())
+        contributors_list = contributors_data
     else:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
@@ -35,19 +57,13 @@ def get_contributors():
             }
             for contributor in contributors_data
         ]
-        redis_conn.setex('github_contributors', 4 * 3600, json.dumps(contributors_list))
+        save_data_to_redis(redis_conn, 'github_contributors', contributors_list, 4 * 3600)
 
     return contributors_list
 
-async def send_result_to_nats(contributors_list):
-    nc = await nats.connect("nats")
-
-    await nc.publish("github_contributors", json.dumps(contributors_list).encode())
-    await nc.close()
-
 async def main():
-    contributors = get_contributors()
-    await send_result_to_nats(contributors)
+    contributors = await get_contributors()
+    await send_data_to_nats("github_contributors", json.dumps(contributors))
 
 if __name__ == '__main__':
     asyncio.run(main())

501afa5cf2138a5c55cc4b87388a5824dd3b0ad6
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-31 19:18:29 +0300

    Добавлена настройка журналирования и обработка ошибок в скрипте запуска парсеров
    Внесены следующие изменения:
    - Добавлен импорт модуля logging для настройки журналирования.
    - Настроено журналирование с уровнем INFO и форматированием.
    - Изменен способ запуска парсеров: теперь они запускаются в цикле с учетом обработки ошибок.
    - Добавлен журналирование начала и завершения работы каждого парсера.
    - Обработка ошибок в каждом парсере с записью сообщения об ошибке в журнал.

--- parsers/run_parsers.py
+++ parsers/run_parsers.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 import approved_issues_parser
 import closed_issues_parser
 import contributors_parser
@@ -9,18 +10,36 @@ import issue_stats_parser
 import issue_type_parser
 import labels_stats_parser
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+
 async def run_parsers():
-    await asyncio.gather(
-        approved_issues_parser.main(),
-        closed_issues_parser.main(),
-        contributors_parser.main(),
-        issue_comment_parser.main(),
-        issue_parser.main(),
-        issue_rewards_parser.main(),
-        issue_stats_parser.main(),
-        issue_type_parser.main(),
-        labels_stats_parser.main()
-    )
+    parsers = [
+        approved_issues_parser,
+        closed_issues_parser,
+        contributors_parser,
+        issue_comment_parser,
+        issue_parser,
+        issue_rewards_parser,
+        issue_stats_parser,
+        issue_type_parser,
+        labels_stats_parser,
+    ]
+
+    total_parsers = len(parsers)
+    completed_parsers = 0
+
+    for parser in parsers:
+        logger = logging.getLogger(parser.__name__)
+        try:
+            logger.info(f"Starting {parser.__name__} ({completed_parsers}/{total_parsers})")
+            await parser.main()
+            completed_parsers += 1
+            logger.info(f"Finished {parser.__name__} ({completed_parsers}/{total_parsers})")
+        except Exception as e:
+            logger.error(f"Error in {parser.__name__}: {str(e)}")
 
 if __name__ == '__main__':
     asyncio.run(run_parsers())

ff4e8a15dd9db0a3b7d00e3151a6cf207d52293b
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-31 19:12:27 +0300

    Refactor issue_rewards_parser.py
    - Moved the 'GITHUB_TOKEN' retrieval into a function 'get_github_token'.
    - Reorganized the 'main' function to improve code readability.
    - Added functions 'save_data_to_redis' and 'get_data_from_redis' to handle data caching in Redis.
    - Introduced asynchronous NATS messaging with 'send_data_to_nats' function.
    - Updated the 'main' function to use the new functions and simplified the code.

--- parsers/issue_rewards_parser.py
+++ parsers/issue_rewards_parser.py
@@ -5,41 +5,58 @@ import json
 import nats
 import redis
 
-redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+def get_github_token():
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+    if GITHUB_TOKEN is None:
+        raise ValueError("Environment variable GITHUB_TOKEN is not set.")
+    
+    return GITHUB_TOKEN
 
-if GITHUB_TOKEN is None:
-    raise ValueError("Environment variable GITHUB_TOKEN is not set.")
-
-HEADERS = {
-    "Authorization": f"Bearer {GITHUB_TOKEN}",
-    "Accept": "application/vnd.github.v3+json"
-}
-
-url = "https://api.github.com/repos/ton-society/grants-and-bounties/issues"
-
-params = {
-    "per_page": 100,
-    "state": "all"
-}
-
-def get_issues(page):
+def get_issues(page, url, params, headers):
     params["page"] = page
-    response = requests.get(url, headers=HEADERS, params=params)
+    response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
     return response.json()
 
+def save_data_to_redis(redis_client, key, data, expiration):
+    redis_client.setex(key, expiration, json.dumps(data))
+
+def get_data_from_redis(redis_client, key):
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None
+
+async def send_data_to_nats(topic, data):
+    nc = await nats.connect("nats")
+    await nc.publish(topic, data.encode())
+    await nc.close()
+
 async def main():
-    cached_data = redis_client.get("issue_rewards")
+    GITHUB_TOKEN = get_github_token()
+    HEADERS = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    url = "https://api.github.com/repos/ton-society/grants-and-bounties/issues"
+    params = {
+        "per_page": 100,
+        "state": "all"
+    }
+
+    redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+
+    cached_data = get_data_from_redis(redis_client, "issue_rewards")
+    
     if cached_data:
-        issue_rewards = eval(cached_data.decode())
+        issue_rewards = cached_data
     else:
         issue_rewards = []
 
         page = 1
         while True:
-            issues = get_issues(page)
+            issues = get_issues(page, url, params, HEADERS)
             if not issues:
                 break
 
@@ -56,13 +73,9 @@ async def main():
 
             page += 1
 
-        redis_client.setex("issue_rewards", 14400, json.dumps(issue_rewards))
+        save_data_to_redis(redis_client, "issue_rewards", issue_rewards, 14400)
 
-    nc = await nats.connect("nats")
-
-    await nc.publish("issue_rewards", issue_rewards.encode())
-
-    await nc.close()
+    await send_data_to_nats("issue_rewards", json.dumps(issue_rewards))
 
 if __name__ == '__main__':
     asyncio.run(main())

d5dee079e91008048a743a951b67df4724a7a920
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-01 00:39:28 +0300

    Refactor issue_type_parser.py: Restructured data storage, added year-wise data, and updated GitHub token handling.

--- parsers/issue_type_parser.py
+++ parsers/issue_type_parser.py
@@ -1,17 +1,9 @@
-import os
 import requests
 import asyncio
 import nats
 import redis
 import json
 
-def get_github_token():
-    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
-
-    if GITHUB_TOKEN is None:
-        raise ValueError("Environment variable GITHUB_TOKEN is not set.")
-    
-    return GITHUB_TOKEN
 
 async def get_issues(session, url, params, page, headers):
     params["page"] = page
@@ -33,8 +25,7 @@ async def send_data_to_nats(topic, data):
     await nc.publish(topic, json.dumps(data).encode())
     await nc.close()
 
-async def main():
-    GITHUB_TOKEN = get_github_token()
+async def main(GITHUB_TOKEN):
     HEADERS = {
         "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json"
@@ -52,10 +43,7 @@ async def main():
     if saved_data:
         result = saved_data
     else:
-        issue_counts = {
-            "open": 0,
-            "closed": 0
-        }
+        issue_counts = {}  # Теперь используем словарь для хранения информации о количестве по годам
 
         with requests.Session() as session:
             page = 1
@@ -66,14 +54,21 @@ async def main():
 
                 for issue in issues:
                     state = issue["state"]
-                    issue_counts[state] += 1
+                    created_at = issue["created_at"][:4]  # Получаем год из даты создания
+                    if created_at not in issue_counts:
+                        issue_counts[created_at] = {"open": 0, "closed": 0}
+                    issue_counts[created_at][state] += 1
 
                 page += 1
 
-        result = [{"state": [{"state": state, "value": count} for state, count in issue_counts.items()]}]
-        save_data_to_redis(redis_client, "issue_type", result, 14400)
+            result = {
+                year: [{"state": state, "value": count} for state, count in issue_counts[year].items()]
+                for year in issue_counts
+            }        
+            save_data_to_redis(redis_client, "issue_type", result, 14400)
 
     await send_data_to_nats("issue_type", result)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    GITHUB_TOKEN="YOUR_GITHUB_TOKEN"
+    asyncio.run(main(GITHUB_TOKEN))

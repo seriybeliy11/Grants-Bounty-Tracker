defbbddc80bff342bf70bc17917724fa021865cc
GoldenEagle74 <mshchannikov@yandex.ru>
2023-10-31 19:11:58 +0300

    Refactor code in issue_parser.py: - Moved the GITHUB_TOKEN retrieval into a function. - Improved error handling for missing GITHUB_TOKEN. - Encapsulated data saving and retrieval to/from Redis. - Modified the data publishing to NATS to use an async function.

--- parsers/issue_parser.py
+++ parsers/issue_parser.py
@@ -5,39 +5,57 @@ import json
 import nats
 import redis
 
-GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
+def get_github_token():
+    GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
-if GITHUB_TOKEN is None:
-    raise ValueError("Environment variable GITHUB_TOKEN is not set.")
-HEADERS = {
-    "Authorization": f"Bearer {GITHUB_TOKEN}",
-    "Accept": "application/vnd.github.v3+json"
-}
+    if GITHUB_TOKEN is None:
+        raise ValueError("Environment variable GITHUB_TOKEN is not set.")
+    
+    return GITHUB_TOKEN
 
-url = "https://api.github.com/repos/ton-society/grants-and-bounties/issues"
-
-params = {
-    "per_page": 100,
-    "state": "all"
-}
-
-def get_issues(page):
+def get_issues(url, params, headers, page):
     params["page"] = page
-    response = requests.get(url, headers=HEADERS, params=params)
+    response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
     return response.json()
 
+def save_data_to_redis(redis_client, key, data, expiration):
+    redis_client.setex(key, expiration, json.dumps(data))
+
+def get_data_from_redis(redis_client, key):
+    data = redis_client.get(key)
+    if data:
+        return json.loads(data)
+    return None
+
+async def send_data_to_nats(topic, data):
+    nc = await nats.connect("nats")
+    await nc.publish(topic, data.encode())
+    await nc.close()
+
 async def main():
+    GITHUB_TOKEN = get_github_token()
+    HEADERS = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    url = "https://api.github.com/repos/ton-society/grants-and-bounties/issues"
+    params = {
+        "per_page": 100,
+        "state": "all"
+    }
+
     r = redis.Redis(host='redis', port=6379, db=0)
 
-    cached_data = r.get("count_issues")
+    cached_data = get_data_from_redis(r, "count_issues")
+    
     if cached_data:
-        result = cached_data.decode('utf-8')
+        result = cached_data
     else:
         issues_by_year = {}
         page = 1
         while True:
-            issues = get_issues(page)
+            issues = get_issues(url, params, HEADERS, page)
             if not issues:
                 break
 
@@ -49,11 +67,9 @@ async def main():
 
         result = [{"Dates": year, "All Issues": count} for year, count in issues_by_year.items()]
 
-        r.setex("count_issues", 14400, json.dumps(result))
+        save_data_to_redis(r, "count_issues", result, 14400)
 
-    nc = await nats.connect("nats")
-    await nc.publish("count_issues", result.encode())
-    await nc.close()
+    await send_data_to_nats("count_issues", json.dumps(result))
 
 if __name__ == '__main__':
     asyncio.run(main())

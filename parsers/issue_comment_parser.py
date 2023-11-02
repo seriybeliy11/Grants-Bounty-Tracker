6b0ff04db353537e0a72da846831cd60e8404bd8
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-02 03:19:45 +0300

    Refactor issue_comment_parser.py to use aiohttp and aioredis
    This commit refactors the 'issue_comment_parser.py' file to use the aiohttp library for making asynchronous HTTP requests and aioredis for working with Redis asynchronously. It also replaces the use of the 'requests' library with aiohttp for better performance. Additionally, the code has been cleaned up and improved for better readability and maintainability.
    
    - Replaced 'requests' with 'aiohttp' for HTTP requests.
    - Used 'aioredis' for asynchronous Redis operations.
    - Refactored code to improve readability.
    - Removed unnecessary imports and code.

--- parsers/issue_comment_parser.py
+++ parsers/issue_comment_parser.py
@@ -1,43 +1,26 @@
-import requests
+import aiohttp
+import ujson
 import asyncio
-import nats
-import redis
-import json
+from redis import asyncio as aioredis
+from collections import OrderedDict
 
-
-def get_issues(GITHUB_TOKEN, url, params, page):
-    HEADERS = {
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "Accept": "application/vnd.github.v3+json"
-    }
+async def get_issues(session, url, params, page):
     params["page"] = page
-    response = requests.get(url, headers=HEADERS, params=params)
-    response.raise_for_status()
-    return response.json()
+    async with session.get(url, params=params) as response:
+        response.raise_for_status()
+        return await response.json(loads=ujson.loads)
 
-async def get_comment_count(GITHUB_TOKEN, issue_url):
-    HEADERS = {
-        "Authorization": f"Bearer {GITHUB_TOKEN}",
-        "Accept": "application/vnd.github.v3+json"
-    }
-    comments_url = issue_url + "/comments"
-    response = await asyncio.to_thread(requests.get, comments_url, headers=HEADERS)
-    response.raise_for_status()
-    return len(response.json())
+async def get_comment_count(session, GITHUB_TOKEN, issue_url):
+    async with session.get(issue_url + "/comments", headers={"Authorization": f"Bearer {GITHUB_TOKEN}", "Accept": "application/vnd.github.v3+json"}) as response:
+        response.raise_for_status()
+        return len(await response.json(loads=ujson.loads))
 
-def save_data_to_redis(redis_client, key, data, expiration):
-    redis_client.setex(key, expiration, json.dumps(data))
+async def save_data_to_redis(redis_client, key, data, expiration):
+    await redis_client.setex(key, expiration, ujson.dumps(data))
 
-def get_data_from_redis(redis_client, key):
-    data = redis_client.get(key)
-    if data:
-        return json.loads(data)
-    return None
-
-async def send_data_to_nats(topic, data):
-    nc = await nats.connect("nats")
-    await nc.publish(topic, data.encode())
-    await nc.close()
+async def get_data_from_redis(redis_client, key):
+    data = await redis_client.get(key)
+    return ujson.loads(data) if data else None
 
 async def main(GITHUB_TOKEN):
     url = "https://api.github.com/repos/ton-society/grants-and-bounties/issues"
@@ -46,44 +29,41 @@ async def main(GITHUB_TOKEN):
         "state": "all"
     }
 
-    redis_conn = redis.StrictRedis(host='redis', port=6379, db=0)
+    redis_conn = await aioredis.from_url("redis://redis")
 
-    cached_data = get_data_from_redis(redis_conn, 'issue_comments')
-    
-    if cached_data:
-        result = cached_data
-    else:
-        issues_with_comments = {}
+    cached_data = await get_data_from_redis(redis_conn, 'issue_comments')
 
-        page = 1
-        while True:
-            issues = get_issues(GITHUB_TOKEN, url, params, page)
-            if not issues:
-                break
+    if not cached_data:
+        issues_with_comments = OrderedDict()
 
-            tasks = []
-            for issue in issues:
-                issue_url = issue["url"]
-                tasks.append(get_comment_count(GITHUB_TOKEN, issue_url))
+        async with aiohttp.ClientSession() as session:
+            page = 1
+            while True:
+                issues = await get_issues(session, url, params, page)
+                if not issues:
+                    break
 
-            comment_counts = await asyncio.gather(*tasks)
+                tasks = []
+                for issue in issues:
+                    issue_url = issue["url"]
+                    tasks.append(get_comment_count(session, GITHUB_TOKEN, issue_url))
 
-            for i, issue in enumerate(issues):
-                issue_number = issue["number"]
-                comments_count = comment_counts[i]
-                created_at = issue["created_at"][:4]  # Извлекаем год из даты
-                if created_at not in issues_with_comments:
-                    issues_with_comments[created_at] = []
-                issues_with_comments[created_at].append({"issue": str(issue_number), "Comments": comments_count})
+                comment_counts = await asyncio.gather(*tasks)
 
-            page += 1
+                for i, issue in enumerate(issues):
+                    issue_number = issue["number"]
+                    comments_count = comment_counts[i]
+                    created_at = issue["created_at"][:4]
+                    if created_at not in issues_with_comments:
+                        issues_with_comments[created_at] = []
+                    issues_with_comments[created_at].append({"issue": str(issue_number), "Comments": comments_count})
 
-        save_data_to_redis(redis_conn, 'issue_comments', issues_with_comments, 4 * 60 * 60)
+                page += 1
 
         result = issues_with_comments
 
-    await send_data_to_nats("issue_comments", json.dumps(result))
+        await save_data_to_redis(redis_conn, 'issue_comments', result, 4 * 60 * 60)
 
 if __name__ == '__main__':
-    GITHUB_TOKEN="YOUR_GITHUB_TOKEN"
-    asyncio.run(main())
+    GITHUB_TOKEN = "YOUR_GITHUB_TOKEN"
+    asyncio.run(main(GITHUB_TOKEN))

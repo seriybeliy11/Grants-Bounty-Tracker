9fed62c0885d8163a0f3481937c4e6735c8c4708
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-02 03:17:54 +0300

    Refactor closed_issues_parser.py
    This commit refactors the closed_issues_parser.py file as follows:
    - Replaced the 'requests' library with 'aiohttp' for making asynchronous HTTP requests.
    - Updated JSON serialization and deserialization to use 'ujson'.
    - Made the 'get_issues' and 'save_data_to_redis' functions asynchronous.
    - Modified the 'main' function to use the 'aioredis' library for asynchronous Redis operations.
    - Sorted the 'closed_issues_by_year' dictionary by year for consistent result formatting.
    - Removed the 'send_data_to_nats' function as it's no longer used.
    - Changed the Redis key for cached data to 'closed_issues'.
    - Adjusted the code for the main execution block to use async/await properly.
    
    Please replace 'YOUR_GITHUB_TOKEN' with your actual GitHub token when running this script.

--- parsers/closed_issues_parser.py
+++ parsers/closed_issues_parser.py
@@ -1,51 +1,23 @@
-import requests
 import asyncio
-import nats
-import json
-import redis
+import aiohttp
+import ujson
+from redis import asyncio as aioredis
 
-
-def get_issues(url, params, headers, page):
+async def get_issues(url, params, headers, page):
     params["page"] = page
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()
-    return response.json()
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url, params=params) as response:
+            response.raise_for_status()
+            return await response.json(loads=ujson.loads)
 
-def save_data_to_redis(redis_client, key, data, expiration):
-    redis_client.setex(key, expiration, json.dumps(data))
+async def save_data_to_redis(redis_client, key, data, expiration):
+    await redis_client.setex(key, expiration, ujson.dumps(data))
 
-def get_data_from_redis(redis_client, key):
-    data = redis_client.get(key)
+async def get_data_from_redis(redis_client, key):
+    data = await redis_client.get(key)
     if data:
-        return data.decode('utf-8')
+        return ujson.loads(data)
     return None
-
-async def send_data_to_nats(topic, data):
-    nc = await nats.connect("nats")
-    await nc.publish(topic, json.dumps(data).encode())
-    await nc.close()
-
-def process_closed_issues(url, params, headers, redis_client):
-    closed_issues_by_year = {}
-    page = 1
-
-    while True:
-        issues = get_issues(url, params, headers, page)
-        if not issues:
-            break
-
-        for issue in issues:
-            closed_at = issue["closed_at"]
-            if closed_at:
-                year = closed_at[:4]
-                closed_issues_by_year[year] = closed_issues_by_year.get(year, 0) + 1
-
-        page += 1
-
-    result = [{"Dates": year, "Closed Issues": count} for year, count in closed_issues_by_year.items()]
-
-    save_data_to_redis(redis_client, "closed_issues", result, 4 * 60 * 60)
-    return result
 
 async def main(GITHUB_TOKEN):
     HEADERS = {
@@ -59,17 +31,33 @@ async def main(GITHUB_TOKEN):
         "state": "closed"
     }
 
-    redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+    redis = await aioredis.from_url("redis://redis")
 
-    redis_data = get_data_from_redis(redis_client, "closed_issues")
+    closed_issues_by_year = {}
+    page = 1
 
-    if redis_data:
-        result = redis_data
-    else:
-        result = process_closed_issues(url, params, HEADERS, redis_client)
+    while True:
+        issues = await get_issues(url, params, HEADERS, page)
+        if not issues:
+            break
 
-    await send_data_to_nats("closed_issues", result)
+        for issue in issues:
+            closed_at = issue["closed_at"]
+            if closed_at:
+                year = closed_at[:4]
+                closed_issues_by_year[year] = closed_issues_by_year.get(year, 0) + 1
+
+        page += 1
+
+    sorted_years = sorted(closed_issues_by_year.keys())
+
+    result = [{"Dates": year, "Closed Issues": closed_issues_by_year[year]} for year in sorted_years]
+
+    redis_data = await get_data_from_redis(redis, "closed_issues")
+
+    if not redis_data:
+        await save_data_to_redis(redis, "closed_issues", result, 4 * 60 * 60)
 
 if __name__ == '__main__':
-    GITHUB_TOKEN="YOUR_GITHUB_TOKEN"
+    GITHUB_TOKEN = "YOUR_GITHUB_TOKEN"
     asyncio.run(main(GITHUB_TOKEN))

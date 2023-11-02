5d1a1da00cd7faa7baafd49ef6e7d385a1fd5997
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-02 03:20:42 +0300

    Refactor code to use aiohttp and ujson for async operations
    This commit refactors the code in  to use aiohttp for asynchronous HTTP requests and ujson for JSON serialization and deserialization. It also replaces the usage: nats [<flags>] <command> [<args> ...]
    
    NATS Utility
    
    NATS Server and JetStream administration.
    
    See 'nats cheat' for a quick cheatsheet of commands
    
    Commands:
      account    Account information and status
      bench      Benchmark utility
      consumer   JetStream Consumer management
      context    Manage nats configuration contexts
      errors     Error code documentation
      events     Show Advisories and Events
      kv         Interacts with a JetStream based Key-Value store
      latency    Perform latency tests between two NATS servers
      micro      Micro Services discovery and management
      object     Interacts with a JetStream based Object store
      publish    Generic data publish utility
      request    Generic request-reply request utility
      reply      Generic service reply utility
      rtt        Compute round-trip time to NATS server
      schema     Schema tools
      server     Server information
      stream     JetStream Stream management
      subscribe  Generic subscription client
    
    Pass --help to see global flags applicable to this command. library with aio-libs for Redis operations.
    
    - Replaced 'import requests' with 'import aiohttp' for asynchronous HTTP requests.
    - Replaced 'import json' with 'import ujson' for faster JSON handling.
    - Updated 'import nats' to aio-libs for asynchronous Redis operations.
    - Modified 'get_issues' and 'save_data_to_redis' functions to be asynchronous.
    - Updated 'get_data_from_redis' to use 'await' for Redis data retrieval.
    - Removed the 'send_data_to_nats' function as it's no longer needed.
    - Updated the 'main' function to work asynchronously with aiohttp and aio-libs.
    
    This commit improves the code's efficiency and enables better performance for handling GitHub API requests and Redis operations.

--- parsers/issue_rewards_parser.py
+++ parsers/issue_rewards_parser.py
@@ -1,29 +1,23 @@
-import requests
+import aiohttp
 import asyncio
-import json
-import nats
-import redis
+import ujson
+from redis import asyncio as aioredis
+from collections import OrderedDict
 
-
-def get_issues(page, url, params, headers):
+async def get_issues(session, page, url, params, headers):
     params["page"] = page
-    response = requests.get(url, headers=headers, params=params)
-    response.raise_for_status()
-    return response.json()
+    async with session.get(url, headers=headers, params=params) as response:
+        response.raise_for_status()
+        return await response.json(loads=ujson.loads)
 
-def save_data_to_redis(redis_client, key, data, expiration):
-    redis_client.setex(key, expiration, json.dumps(data))
+async def save_data_to_redis(redis_client, key, data, expiration):
+    await redis_client.setex(key, expiration, ujson.dumps(data))
 
-def get_data_from_redis(redis_client, key):
-    data = redis_client.get(key)
+async def get_data_from_redis(redis_client, key):
+    data = await redis_client.get(key)
     if data:
-        return json.loads(data)
+        return ujson.loads(data)
     return None
-
-async def send_data_to_nats(topic, data):
-    nc = await nats.connect("nats")
-    await nc.publish(topic, data.encode())
-    await nc.close()
 
 async def main(GITHUB_TOKEN):
     HEADERS = {
@@ -36,43 +30,37 @@ async def main(GITHUB_TOKEN):
         "state": "all"
     }
 
-    redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+    redis_client = await aioredis.from_url("redis://redis")
 
-    cached_data = get_data_from_redis(redis_client, "issue_rewards")
-    
-    if cached_data:
-        issue_rewards = cached_data
-    else:
-        issue_rewards = {}
-        
-        page = 1
-        while True:
-            issues = get_issues(page, url, params, HEADERS)
-            if not issues:
-                break
+    cached_data = await get_data_from_redis(redis_client, "issue_rewards")
 
-            for issue in issues:
-                created_at = issue["created_at"][:4]
-                body = issue.get("body")
-                if body:
-                    reward_info = body.split('$')
-                    if len(reward_info) >= 2:
-                        try:
-                            reward = int(reward_info[1].split(' ')[0])
-                            issue_info = {"Issue Number": str(issue["number"]), "Rewards (th. $)": reward}
-                            if created_at in issue_rewards:
-                                issue_rewards[created_at].append(issue_info)
-                            else:
-                                issue_rewards[created_at] = [issue_info]
-                        except ValueError:
-                            pass
+    if not cached_data:
+        issue_rewards = OrderedDict()
 
-            page += 1
+        async with aiohttp.ClientSession() as session:
+            page = 1
+            while True:
+                issues = await get_issues(session, page, url, params, HEADERS)
+                if not issues:
+                    break
 
-        save_data_to_redis(redis_client, "issue_rewards", issue_rewards, 14400)
+                for issue in issues:
+                    created_at = issue["created_at"][:4]
+                    body = issue.get("body")
+                    if body:
+                        reward_info = body.split('$')
+                        if len(reward_info) >= 2:
+                            try:
+                                reward = int(reward_info[1].split(' ')[0])
+                                issue_info = {"Issue Number": str(issue["number"]), "Rewards (th. $)": reward}
+                                issue_rewards.setdefault(created_at, []).append(issue_info)
+                            except ValueError:
+                                pass
 
-    await send_data_to_nats("issue_rewards", json.dumps(issue_rewards))
+                page += 1
+
+        await save_data_to_redis(redis_client, "issue_rewards", issue_rewards, 14400)
 
 if __name__ == '__main__':
-    GITHUB_TOKEN="YOUR_GITHUB_TOKEN"
+    GITHUB_TOKEN = "YOUR_GITHUB_TOKEN"
     asyncio.run(main(GITHUB_TOKEN))

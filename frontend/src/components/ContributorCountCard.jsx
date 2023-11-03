980b265dc4fedffe49f2e04b73b6da652a43f13a
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-03 20:07:22 +0300

    Update font references and labels in CSS and components

---
+++ frontend/src/components/ContributorCountCard.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { Card, Metric } from "@tremor/react";
+
+const AgentQueCard = () => {
+  const [loginCount, setLoginCount] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:3000/github_contributors");
+        if (!response.ok) {
+          throw new Error('Error');
+        }
+        const jsonData = await response.json();
+        const contributorCount = jsonData.result;
+        setLoginCount(contributorCount.length);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <Card style={{ borderRadius: '16px' }}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <Metric style = {{fontSize: '15px', fontFamily:'Manrope-900'}}>Contributor's Value</Metric>
+      </div>
+      <Metric style={{ fontSize: '27px', fontFamily: 'Manrope-900' }}>
+        {loginCount !== null ? loginCount : "Loading..."}
+      </Metric>
+    </Card>
+  );
+};
+
+export default AgentQueCard;

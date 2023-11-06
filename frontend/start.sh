d19667ad0e03b29fc3b9c343bf6b5e1c1e00c213
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-06 21:13:46 +0300

    removed debug

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-echo starting api server
-
 node server.js &
 
 while true; do
-  echo requesting to api server
   response=$(curl -s "http://localhost:3000/check_status")
-  echo parsing response
   status=$(echo "$response" | grep -o '"status":"[^"]*"' | awk -F ':"' '{print $2}' | awk -F '"' '{print $1}')
   if [ "$status" = "completed" ]; then
     break
   fi
   sleep 1
 done
-
-echo starting frontend
 
 npm run dev -- --host

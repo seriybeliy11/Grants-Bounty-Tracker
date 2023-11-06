0d5a8ded080677a070c0f9d406a3b129059523e2
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-06 20:52:56 +0300

    added debug

--- frontend/start.sh
+++ frontend/start.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+echo starting api server
+
 node server.js &
 
 while true; do
+  echo requesting to api server
   response=$(curl -s "http://localhost:3000/check_status")
+  echo parsing response
   status=$(echo "$response" | grep -o '"status":"[^"]*"' | awk -F ':"' '{print $2}' | awk -F '"' '{print $1}')
   if [ "$status" = "completed" ]; then
     break
   fi
   sleep 1
 done
+
+echo starting frontend
 
 npm run dev -- --host

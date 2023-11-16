b7e1971e3cbad858b0f31fef0767b023f70f3b79
GoldenEagle74 <mshchannikov@yandex.ru>
2023-11-16 17:58:55 +0300

    changed refresh time to 15 min

--- parsers/run_parsers.py
+++ parsers/run_parsers.py
@@ -62,7 +62,7 @@ def schedule_run_parsers():
 if __name__ == '__main__':
     asyncio.run(run_parsers())
 
-    schedule.every(5).minutes.do(schedule_run_parsers)
+    schedule.every(15).minutes.do(schedule_run_parsers)
 
     while True:
         schedule.run_pending()
